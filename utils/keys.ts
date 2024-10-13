b73022d92263e4c57e1fca8a0fab9b702f899e0e
Nicolás Navarro <nicohnavarro@gmail.com>
2024-10-13 10:52:44 +0200

    🔑 Fix key

--- utils/keys.ts
+++ utils/keys.ts
@@ -2,7 +2,7 @@ export function getKeyName(...args: string[]) {
   return `bites:${args.join(":")}`;
 }
 
-export const restaurantKeyById = (id: string) => getKeyName("restaurant", id);
+export const restaurantKeyById = (id: string) => getKeyName("restaurants", id);
 export const reviewKeyById = (id: string) => getKeyName("review", id);
 export const reviewDetailsKeyById = (id: string) =>
   getKeyName("review_details", id);
